4f410ed6e4e9850477ca1d38d56f8c7b8a5fd972
Anatolii <54738485+avolotovskii@users.noreply.github.com>
2022-05-12 20:50:52 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
     $('.carousel__inner').slick({
         speed: 1300,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icon/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icon/right.svg"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
         responsive: [{
             breakpoint: 992,
             settings: {
